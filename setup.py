2f6bc71000f66607ef5e29a5049c1b9e623a019f
Pierre Wacrenier <mota@souitom.org>
2016-01-20 01:43:06 +0100

    Refactoring, tree manipulation in i3_tree

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description="Cycle focus through i3 containers",
     url="http://github.com/mota/i3-cycle",
     license="ISC",
-    install_requires=["i3-py==0.6.4"],
+    install_requires=["i3-tree"],
     py_modules=["i3_cycle"],
     entry_points={
         "console_scripts": [
