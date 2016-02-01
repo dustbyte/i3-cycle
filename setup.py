510442ef1d51f0366d45ea167058e97a0152a99d
Pierre Wacrenier <mota@souitom.org>
2016-02-01 23:53:21 +0100

    Bump version 1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="i3-cycle",
-    version="0.6",
+    version="1.0",
     author="Pierre Wacrenier",
     author_email="mota@souitom.org",
     description="Cycle focus through i3 containers",
